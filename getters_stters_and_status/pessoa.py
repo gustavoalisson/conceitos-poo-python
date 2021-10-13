3a615e8e6881e35e6f260465ad32bf5372242982
gustavoalisson <52181576+gustavoalisson@users.noreply.github.com>
2021-10-13 17:33:10 -0300

    getter e setter python sem ser do jeito pythonico

---
+++ getters_stters_and_status/pessoa.py
@@ -0,0 +1,16 @@
+
+# ! Importe criar nomes de classe / atributos com substantivos e nomes de metodos com verbos
+
+class Pessoa:
+    def __init__(self, nome: str, idade: int) -> None:
+        self.nome = nome
+        self.idade = idade
+    
+    def dirigir(self, veiculo: str) -> None:
+        print(f'Dirigindo um(a) {veiculo}')
+    
+    def cantar(self) -> None: 
+        print('cantando... lalala')
+    
+    def apresentar_idade(self) -> int:
+            return self.idade
