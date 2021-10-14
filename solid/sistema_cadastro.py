b8ef91ea312d649bce9089aba99ab5e9c24d268c
gustavoalisson <52181576+gustavoalisson@users.noreply.github.com>
2021-10-14 10:28:53 -0300

    Implementação de exemplo para entender principios de SOLID

---
+++ solid/sistema_cadastro.py
@@ -0,0 +1,27 @@
+# Princípio da responsabilidade única
+
+class SistemaCadastral:
+    
+    def cadastrar(self, nome: str, idade: int) -> None:
+        if self.__verificar_dados(nome, idade):
+            self.__armazenar_usuario(nome, idade)
+        else:
+            self.__indicar_erro()
+            
+    def __verificar_dados(self, nome: str, idade: int) -> bool:
+        if isinstance(nome, str) and isinstance(idade, int):
+            return True
+        else:
+            return False
+    
+    def __armazenar_usuario(self, nome: str, idade: int) -> bool:
+        print('acessando o banco...')
+        print(f'Cadastrar o usuário {nome}, idade {idade}')
+    
+    def __indicar_erro(self) -> None:
+        print('Dados inválidos!!')    
+            
+sistema = SistemaCadastral()
+sistema.cadastrar('Alisson', 24)
+        
+                    
