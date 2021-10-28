74c079f2eb4cd4292d6dd6a14719bf759baa4892
gustavoalisson <52181576+gustavoalisson@users.noreply.github.com>
2021-10-28 11:11:49 -0300

    Injeção de dependencia em python

--- injecao_dependencia/pessoa.py
+++ injecao_dependencia/pessoa.py
@@ -1,3 +1,4 @@
+from typing import Type
 class Casa:
     def __init__(self, dono_casa) -> None:
         self.__dono_casa = dono_casa
@@ -9,19 +10,28 @@ class Casa:
     def get_endereco(self) -> str:
         return self.__endereco
     
+    def proprietario(self) -> str:
+        return self.__dono_casa
+    
 class Pessoa:
-    def __init__(self, nome: str) -> None:
+    def __init__(self, nome: str, local: Type[Casa]) -> None:
+        self.__local = local
         self.__nome = nome
     
-    def entrar_no_local(self, local: any) -> None:
-        local.acender_luzes()
+    def entrar_no_local(self) -> None:
+        self.__local.acender_luzes()
     
-    def apresentar_local(self, local: any) -> None:
-        endereco = local.get_endereco()
+    def apresentar_local(self) -> None:
+        endereco = self.__local.get_endereco()
         print(endereco)
+    
+    def apresentar_proprietario(self) -> None:
+        prop = self.__local.proprietario()
+        print(f'Dono da casa: {prop}')    
 
-casa_amigo = Casa('Luis')
-pessoa = Pessoa('Alisson')        
+casa_amigo = Casa('Luiz')
+pessoa = Pessoa('Alisson', casa_amigo)        
 
-pessoa.entrar_no_local(casa_amigo)
-pessoa.apresentar_local(casa_amigo)
+pessoa.entrar_no_local()
+pessoa.apresentar_proprietario()
+pessoa.apresentar_local()
